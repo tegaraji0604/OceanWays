49f651a2e3c080f48aecab7974db73f9f50153d6
tegaraji0604 <ffaraider22@gmail.com>
2024-06-10 14:43:32 +0700

    Database

---
+++ BAB 6/OceanWays/dashboard.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" href="dashboard.css">
+</head>
+<body>
+
+<div class="sidebar">
+   
+    <ul>
+        <h1> =Dashboard=</h1>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="transaction.php">Pemesanan</a></li>
+        <li><a href="logout.php" >Logout</a></li>
+    </ul>
+</div>
+
+<div class="content">
+    <h2>Welcome to My Dashboard</h2>
+
+</div>
+
+</body>
+</html>
